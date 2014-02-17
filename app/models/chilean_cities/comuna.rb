f2695874e3fcdf57d7fab8486359d2967aecc70d
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-02-17 16:21:31 -0300

    Add Rails-3-compatible strong parameters support
    Patch idea: when attributes accessible is not required, it's no
    longer included.

--- app/models/chilean_cities/comuna.rb
+++ app/models/chilean_cities/comuna.rb
@@ -1,5 +1,21 @@
 module ChileanCities
   class Comuna < ActiveRecord::Base
-    attr_accessible :name, :code, :provincia, :region, :region_number
+
+    # See https://github.com/plataformatec/devise/blob/3232d14b20da4a596f2c0ff31b8a9ac0a529322e/lib/generators/devise/orm_helpers.rb
+    def self.needs_attr_accessible?
+      rails_3? && !strong_parameters_enabled?
+    end
+
+    def self.rails_3?
+      Rails::VERSION::MAJOR == 3
+    end
+
+    def self.strong_parameters_enabled?
+      defined?(ActionController::StrongParameters)
+    end
+
+    if needs_attr_accessible?
+      attr_accessible :name, :code, :provincia, :region, :region_number
+    end
   end
 end
