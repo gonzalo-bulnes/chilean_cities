d45f5f0c4f8c627c2270bbe374d6aad13ca6352c
juliogarciag <julioggonz@gmail.com>
2014-05-22 11:26:20 -0500

    adding decorator support

--- lib/chilean_cities/engine.rb
+++ lib/chilean_cities/engine.rb
@@ -9,5 +9,10 @@ module ChileanCities
       g.helper false
     end
 
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
