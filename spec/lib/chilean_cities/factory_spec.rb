d0dba189820075b1589c7d16951662569480693d
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-03-17 21:23:50 -0300

    Add failing specs for Factory#generate!

--- spec/lib/chilean_cities/factory_spec.rb
+++ spec/lib/chilean_cities/factory_spec.rb
@@ -17,5 +17,30 @@ describe ChileanCities::Factory do
     it 'generates Comuna, Provincia and Region objects from the data' do
       expect(subject.generate!).to eq({ comunas_count: 346, provincias_count: 54, regiones_count: 15 })
     end
+
+    describe 'generates a collection of' do
+
+      it 'valid Regiones instances' do
+        subject.regiones.each do |region|
+          expect(region).to be_valid
+        end
+        subject.generate!
+      end
+
+      it 'valid Provincias instances' do
+        subject.provincias.each do |provincia|
+          expect(provincia).to be_valid
+        end
+        subject.generate!
+      end
+
+      it 'valid Comunas instances' do
+        subject.comunas.first.valid?
+        subject.comunas.each do |comuna|
+          expect(comuna).to be_valid
+        end
+        subject.generate!
+      end
+    end
   end
 end
